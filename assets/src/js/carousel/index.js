cf811baffec0ff68d115e2b9425af6b2afadad88
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-08-24 13:48:35 -0400

    #82 Responsive Carousel

--- assets/src/js/carousel/index.js
+++ assets/src/js/carousel/index.js
@@ -8,9 +8,35 @@
             console.log("Fuck");
             $('.posts-carousel').slick({
                 autoplay: true,
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1
+                autoplay:1000,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                responsive: [
+                    {
+                      breakpoint: 768,
+                      settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                      }
+                    },
+                    {
+                      breakpoint: 600,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                      }
+                    },
+                    {
+                      breakpoint: 480,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                      }
+                    }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+              ]
             });
         }
     }
