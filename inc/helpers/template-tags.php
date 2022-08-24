8303ca46f5663c2600e0fdfb8cc9dcaeeb9adc47
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-08-24 18:36:33 -0400

    #85 Custom Search Results Page

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -1,18 +1,22 @@
 <?php
 
 //used within Wordpress Loop
-function get_the_post_custom_thumbnail($post_id, $size = "featured-image", $additional_attributes =[]){
+function get_the_post_custom_thumbnail($post_id, $size = "featured-image", $additional_attributes = [])
+{
     //get the post id, size and additional attributes passed as an array
     $custom_thumbnail = "";
     //if there is no post id passed in
-    if($post_id === null){
+    if ($post_id === null) {
         $post_id = get_the_ID();
     }
 
-    if(has_post_thumbnail($post_id)){
+    if (has_post_thumbnail($post_id)) {
         $default_attributes = [
             'loading' => 'lazy'
         ];
+    } else {
+        //prevents an error in the array merge
+        $default_attributes = [];
     }
     //Merges the elements of one or more arrays together so that the values of 
     //one are appended to the end of the previous one. 
@@ -42,64 +46,68 @@ function get_the_post_custom_thumbnail($post_id, $size = "featured-image", $addi
  * @param array $additional_attributes
  */
 
-function the_post_custom_thumbnail($post_id, $size = "featured-thumbnail", $additional_attributes =[]){
+function the_post_custom_thumbnail($post_id, $size = "featured-thumbnail", $additional_attributes = [])
+{
     echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
 }
 
-function aquila_posted_on(){
+function aquila_posted_on()
+{
     $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
     //check if the current time is not equal to the modified time
     //wp stores modified time in db
     //if the current time is not the same as the stored modified time, assume post has been modified
-    if(get_the_time("U") !== get_the_modified_time("U")){
+    if (get_the_time("U") !== get_the_modified_time("U")) {
         $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
     }
 
     //use string formatting to populate time string
     //use DATE_W3C format for Seo related dates
 
-    $time_string = sprintf($time_string,
+    $time_string = sprintf(
+        $time_string,
         esc_attr(get_the_date(DATE_W3C)),
         esc_attr(get_the_date()),
         esc_attr(get_the_modified_date(DATE_W3C)),
         esc_attr(get_the_modified_date())
-);
+    );
     //now use the time string in copy
 
     $posted_on = sprintf(
         esc_html_x('Posted on %s', 'post date', 'aquila'),
-        '<a href="' . esc_url(get_permalink()) . '" style="text-decoration:none" rel="bookmark">' .$time_string. '</a>'
+        '<a href="' . esc_url(get_permalink()) . '" style="text-decoration:none" rel="bookmark">' . $time_string . '</a>'
     );
 
-    echo '<span class="posted-on text-secondary">'. $posted_on . '</span>';
-
+    echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
 }
 
-function aquila_posted_by(){
+function aquila_posted_by()
+{
     //get author name
     //link to the authorpage
     //need the author ID in order to do this
     //use the get_the_author_meta() with the ID parameter
     $byline = sprintf(
         esc_html_x('by %s', 'post author', "aquila"),
-        "<span class='author vcard'><a href='" . esc_url(get_author_posts_url(get_the_author_meta("ID") ))."'>".esc_html(get_the_author())."</a></span>"
+        "<span class='author vcard'><a href='" . esc_url(get_author_posts_url(get_the_author_meta("ID"))) . "'>" . esc_html(get_the_author()) . "</a></span>"
     );
-    echo '<span class="byline text-secondary">'.$byline.'</span>';
+    echo '<span class="byline text-secondary">' . $byline . '</span>';
 }
 // check if there is an excerpt 
 // if no excerpt, generate one, else use the one that is there
-function aquila_the_excerpt ($trim_character_count = 0){
+function aquila_the_excerpt($trim_character_count = 0)
+{
 
     $post_ID = get_the_ID();
 
-	if ( empty( $post_ID ) ) {
-		return null;
-	}
+    if (empty($post_ID)) {
+        return null;
+    }
 
     //check if the excerpt has been set or trimmed to zero
     //by default, this will be 55 characters
-    if(! has_excerpt() || $trim_character_count === 0 ){
-        the_excerpt();//uses get_the_excerpt()
+    if (!has_excerpt() || $trim_character_count === 0) {
+        the_excerpt(); //uses get_the_excerpt()
         //$excerpt = substr($excerpt,0, strrpos($excerpt, " "));
 
         return;
@@ -111,19 +119,19 @@ function aquila_the_excerpt ($trim_character_count = 0){
     //now cut down text to first space by reprocessing the string, using 
     //strrpos to find last space to remove the last word
     //so you do not end up with half a word
-    $excerpt = substr($excerpt,0, strrpos($excerpt, " "));
-    echo $excerpt ;
-    
+    $excerpt = substr($excerpt, 0, strrpos($excerpt, " "));
+    echo $excerpt;
 }
 
-function aquila_excerpt_more($more = ""){
+function aquila_excerpt_more($more = "")
+{
     //dont show on a single page
 
-    if(! is_single()){
+    if (!is_single()) {
         $more = sprintf(
             '<a href="%1$s" class="aquila-read-more "><button class="mt-4 mb-3 btn btn-info text-white">%2$s</button></a>',
             get_permalink(get_the_ID()),
-            __("Read More",'aquila')
+            __("Read More", 'aquila')
         );
     }
 
@@ -131,7 +139,8 @@ function aquila_excerpt_more($more = ""){
 }
 
 
-function  aquila_pagination(){
+function  aquila_pagination()
+{
     //will show the next article in loop
     //wrap pagination in a nav tag
     //use kses to strip out all but the allowed protocols
@@ -151,11 +160,13 @@ function  aquila_pagination(){
             "class" => [],
             "href" => [],
         ]
-        
-    ];
-   printf('<nav class="aquila-pagination clearfix">%s</nav>',
-        wp_kses(paginate_links($args),
-            $allowed_tags)
-);
 
+    ];
+    printf(
+        '<nav class="aquila-pagination clearfix">%s</nav>',
+        wp_kses(
+            paginate_links($args),
+            $allowed_tags
+        )
+    );
 }
--- index.php
+++ index.php
@@ -8,7 +8,9 @@
  * @package Aquila
  */
 get_header("");
- ?>
+
+
+?>
 
 
 <!--[if lt IE 7]>
@@ -17,56 +19,56 @@ get_header("");
 <div id="primary">
     <main id="main" class="site-main mt-5" role="main">
         <?php
-        if(have_posts()) :
-            ?>
+        if (have_posts()) :
+        ?>
             <div class="container">
-            <?php
-            //the determins if you are on the blog home page
-            //The blog homepage is the page that shows the time-based blog content of the site.
-            //is_front_page()- Determines whether the query is for the front page of the site.
-            //This is for what is displayed at your site's main URL.
-                if( is_home() && ! is_front_page() ){
+                <?php
+                //the determins if you are on the blog home page
+                //The blog homepage is the page that shows the time-based blog content of the site.
+                //is_front_page()- Determines whether the query is for the front page of the site.
+                //This is for what is displayed at your site's main URL.
+                if (is_home() && !is_front_page()) {
                 ?>
-                <header class="mb-5">
-                    <h1 class="page-title">
-                        <?php 
-                        //Display or retrieve page title for post.
-                        single_post_title(); 
-                        ?>
-                    </h1>
-                </header>
+                    <header class="mb-5">
+                        <h1 class="page-title">
+                            <?php
+                            //Display or retrieve page title for post.
+                            single_post_title();
+                            ?>
+                        </h1>
+                    </header>
                 <?php
                 }
                 ?>
                 <div class="row">
                     <?php
-                        //set up front page grid so that there are 3 posts per div
-                        $index = 0;
-                        $no_of_articles = 3;
-                        // start loop
-                        while( have_posts()) : the_post();
-                            //prints a column header for every 3 articles
-                            if($index % $no_of_articles === 0){
-                                ?>
-                                <div class="col-lg-4 col-md-6 col-sm-12">
-                                
-                                <?php
-                                }
-                               
-                              get_template_part("template-parts/content");
-                        
-                            $index ++;
+                    //set up front page grid so that there are 3 posts per div
+                    $index = 0;
+                    $no_of_articles = 3;
+                    // start loop
+                    while (have_posts()) : the_post();
+                        //prints a column header for every 3 articles
+                        if ($index % $no_of_articles === 0) {
+                    ?>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
 
-    
+                            <?php
+                        }
+
+                        get_template_part("template-parts/content");
+
+                        $index++;
+
+
                         //when index isnt zero and we hit article limit for column
                         //close the div
-                        if( $index !==0  && $index%$no_of_articles === 0){
-                        ?>
+                        if ($index !== 0  && $index % $no_of_articles === 0) {
+                            ?>
                             </div>
-                        <?php
+                    <?php
                         }
-                        endwhile; 
-                        ?>
+                    endwhile;
+                    ?>
                 </div>
             </div>
         <?php
@@ -74,9 +76,9 @@ get_header("");
             get_template_part("template-parts/content", "none");
 
         endif;
-         //pagination function, found in template-tags file //pagination function, found in template-tags file
+        //pagination function, found in template-tags file //pagination function, found in template-tags file
         aquila_pagination();
-      
+
         ?>
 
     </main>
@@ -87,6 +89,3 @@ get_header("");
 
 <?php
 get_footer();
-
-    
-   
