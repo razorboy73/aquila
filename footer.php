460d1de26b07ac5b0e382441e20162c5d98b89b2
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-08-27 13:41:51 -0400

    #86 adding SVG with use

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
  * @package Aquila
  */
 
- 
- /* widget process involves:
+
+/* widget process involves:
  * 1. extending the class
  * 2. register widget with register_widget(Name of Class )
  * 3. add_action('widgets_init', function)
@@ -16,28 +16,65 @@
 
 
 
- ?>
+?>
 </div>
 
 </div>
-      <script src="" async defer></script>
-    <footer>
-      
-    <h3>Footer</h3>
-    <?php
-    //checkif sidebar is active and if so call it
-        if(is_active_sidebar('sidebar-2')){
+<script src="" async defer></script>
+<footer id="site-footer" class="bg-light p-4">
+
+  <div class="container color-gray">
+    <div class="row">
+      <section class="col-lg-4 col-md-6 col-sm-12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quis repellat tempore, magni corporis ex quibusdam repellendus velit nostrum laboriosam explicabo, harum ea quidem! Aliquam quas tempore vitae quia tenetur.
+      </section>
+
+      <section class="col-lg-4 col-md-6 col-sm-12">
+
+        <?php
+        //checkif sidebar is active and if so call it
+        if (is_active_sidebar('sidebar-2')) {
         ?>
-      <aside>
+          <aside>
 
-      
-      <?php dynamic_sidebar('sidebar-2'); ?>
-      </aside>
 
-      <?php
-    }
-    ?>
-    </footer>
-    <?php wp_footer(); ?>
-    </body>
- </html>
+            <?php dynamic_sidebar('sidebar-2'); ?>
+          </aside>
+
+        <?php
+        }
+        ?>
+      </section>
+      <section class="col-lg-4 col-md-6 col-sm-12">
+        <ul class="d-flex">
+          <li class="list-unstyled"><a href="https://facebook.com" title="facebook">
+              <svg width="48">
+                <use href="#icon-facebook"></use>
+              </svg>
+
+            </a></li>
+          <li class="list-unstyled"><a href="https://twitter.com" title="twitter">
+              <svg width="48">
+                <use href="#icon-twitter"></use>
+              </svg>
+
+            </a></li>
+          <li class="list-unstyled"><a href="https://linkedin.com" title="linkedin">
+              <svg width="48">
+                <use href="#icon-linkedin"></use>
+              </svg>
+            </a></li>
+        </ul>
+
+      </section>
+    </div>
+  </div>
+
+</footer>
+<?php
+get_template_part('template-parts/content', 'svgs');
+
+wp_footer(); ?>
+</body>
+
+</html>
